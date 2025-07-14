ae94674cb45a68e7ec90e1d8eb69128751010a30
Jayesh Suryawanshi <jps25102001@gmail.com>
2025-07-14 10:05:37 +0530

    Updated script to add telegram notification support

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+TELEGRAM_BOT_TOKEN = "TOKEN"
+TELEGRAM_CHAT_ID = "CHAT_ID"
+
 PRODUCTS = {
     "Plain Lassi": "https://shop.amul.com/en/product/amul-high-protein-plain-lassi-200-ml-or-pack-of-30",
     "Rose Lassi": "https://shop.amul.com/en/product/amul-high-protein-rose-lassi-200-ml-or-pack-of-30"
@@ -17,18 +20,27 @@ def check_stock(url):
 
     if button and "Add to Cart" in button.text:
         return True
-
     return False
+
+
+def send_telegram_alert(product, url):
+    message = f"{product} is back in stock!\n{url}"
+
+    api = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message
+    }
+
+    requests.post(api, data=data)
 
 
 while True:
     for name, url in PRODUCTS.items():
-        available = check_stock(url)
 
-        if available:
-            print(f"{name} is available")
-        else:
-            print(f"{name} is out of stock")
+        if check_stock(url):
+            print("Sending alert")
+            send_telegram_alert(name, url)
 
-    print("Sleeping...")
     time.sleep(60)
